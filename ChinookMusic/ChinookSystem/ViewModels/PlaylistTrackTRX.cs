4a89a047b96157cb1bc429219cc9c7d2d51e0da8
gurkirat121 <gurkiratsingh9812@gmail.com>
2022-10-28 20:41:38 -0600

    vs

---
+++ ChinookMusic/ChinookSystem/ViewModels/PlaylistTrackTRX.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.ViewModels
+{
+    public class PlaylistTrackTRX
+    {
+        public bool SelectedTrack { get; set; }
+        public int TrackId { get; set; }
+        public int TrackNumber { get; set; }
+        public int TrackInput { get; set; }
+
+    }
+}
